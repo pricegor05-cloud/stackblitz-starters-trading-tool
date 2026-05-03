e37eb1c8e103e765f5ae051fb00550cbbe5866d7
pricegor05-cloud <pricegor05@gmail.com>
2026-05-02 22:27:14 -0500

    modified:   alpha_engine.py

--- alpha_engine.py
+++ alpha_engine.py
@@ -66,9 +66,9 @@ def alpha_engine(market_data):
         }
 
     return signals
-    print(type(rsi_val), rsi_val)
-    print(type(vwap_val), vwap_val)
-    def last_value(x):
-    return float(x.dropna().iloc[-1])
-     rsi_val = last_value(rsi_series)
-     vwap_val = last_value(vwap_series)
+print(type(rsi_val), rsi_val)
+print(type(vwap_val), vwap_val)]
+def last_value(x):
+return float(x.dropna().iloc[-1])
+rsi_val = last_value(rsi_series)
+vwap_val = last_value(vwap_series)
