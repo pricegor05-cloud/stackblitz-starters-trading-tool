0d43789aad9880df770b2a71221d4223060224ec
pricegor05@gmail.com <pricegor05@gmail.com>
2026-05-02 22:51:19 -0500

    fix Series to float conversion bug

--- alpha_engine.py
+++ alpha_engine.py
@@ -74,3 +74,8 @@ def alpha_engine(market_data):
         }
 
     return signals
+    def last(x):
+    return float(x.dropna().iloc[-1])
+    price = last(df["Close"])
+    rsi_val = last(rsi_series)
+    vwap_val = last(vwap_series)       
